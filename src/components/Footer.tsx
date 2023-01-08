6e1a26772efb0ee496f38ce10f07adf0aa3b3cc0
Mohammad-Gharbi <91133928+Mohammad-Gharbi@users.noreply.github.com>
2023-01-08 16:35:20 +0100

    init

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div className="flex items-center justify-center p-8 bottom-0">
+      <div className="text-gray-400">© 2023 — made by Mohamed G.</div>
+    </div>
+  )
+}
+
+export default Footer
